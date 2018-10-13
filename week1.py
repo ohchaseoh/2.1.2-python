4be5120215a63c789e079919421bd57dc0931fa6
youngcalculate42 <aceohmstede@gmail.com>
2018-10-13 14:16:43 -0700

    Add files via upload

---
+++ week1.py
@@ -0,0 +1,17 @@
+x = "#   #" #String
+y = 0 #int
+i = 100 #final int
+z = i #int
+a = "" #String
+for y in range (i):
+    while(z>y):
+        a = a + " ";
+        z = z - 1;
+    print(a + x);
+    x = "#" + x + "#";
+    a = "";
+    z = i;
+    y = y + 1;
+    
+        
+    
